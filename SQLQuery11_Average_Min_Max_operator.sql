8f401bf2c09192895711a19d6e36899b65cccf95
dananjayawickrama95@gmail.com <dananjayawickrama95@gmail.com>
2021-07-13 11:39:58 +0530

    I uploaded

--- SQLQuery11_Average_Min_Max_operator.sql
+++ SQLQuery11_Average_Min_Max_operator.sql
@@ -4,5 +4,6 @@
 --// Find the average, minimum, maximum gpa of students?
 
 select AVG(gpa) as AVERAGE,MAX(gpa) as MAXIMUM,MIN(gpa) as MINIMUM
-from Table_1_Student;  
+from Table_1_Student; 
+------ 
 
